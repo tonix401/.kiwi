56ac6fc1dbb6f6b028b93481c9754bf62b6f1f3b
Tom Weise <tom.weise.2004@gmail.com>
2025-11-18 19:38:47 +0100

    idk

--- hypr/hooks.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-function handle {
-  case $1 in
-    "openwindow"*) notify-send "Window opened" ;;
-  esac
-}
-
-socat - "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" | while read -r line; do handle "$line"; done
